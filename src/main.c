535aa51ffb078aec94a56bf9fb186b6aea2f726a
Cameron Moy <camoy@umd.edu>
2016-03-22 22:41:45 -0400

    Add XML namespace flag

--- src/main.c
+++ src/main.c
@@ -7,14 +7,24 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+	int ns = 0;
 	char buffer[BUF_SIZE];
 	char *content = "";
+
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "--xml-ns"))
+			ns = 1;
+	}
 
 	while (fgets(buffer, BUF_SIZE, stdin))
 		asprintf(&content, "%s%s", content, buffer);
 
 	char *result = amath_to_mathml(content);
-	printf("<math xmlns=\"http://www.w3.org/1998/Math/MathML\">%s</math>", result);
+	if (!ns)
+		printf("<math>%s</math>", result);
+	else
+		printf("<math xmlns=\"http://www.w3.org/1998/Math/MathML\">%s</math>", result);
 
 	if (strlen(result) > 0) free(result);
 	if (strlen(content) > 0) free(content);
